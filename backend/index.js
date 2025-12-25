c0a5c82ab29c3123276841ddc70e34b95c2ed3d1
akshatchoukse <choukseakshat@gmail.com>
2025-12-25 12:42:34 +0530

    Model created and mongoDb connected

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 dotenv.config()
 const PORT = process.env.PORT || 8080
 const app = express()
+app.use(express.json())
+connectDB()
 
 app.listen(PORT,()=>{
     console.log(`Server Started on http://localhost:${PORT}`)
