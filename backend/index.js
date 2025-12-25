49c6d8fd0437b2a1551b06ca8a6d7b8144c10a49
akshatchoukse <choukseakshat@gmail.com>
2025-12-25 12:30:39 +0530

    Server Started

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+const PORT = process.env.PORT || 8080
+const app = express()
+
+app.listen(PORT,()=>{
+    console.log(`Server Started on http://localhost:${PORT}`)
+})
