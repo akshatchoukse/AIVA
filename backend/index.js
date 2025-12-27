b332eb38985b8fe121bdf7568a142b66c9aa2468
akshatchoukse <choukseakshat@gmail.com>
2025-12-28 03:06:09 +0530

    Cors editted

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 8080
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 connectDB()
 app.use("/api/auth",authRouter)
 
